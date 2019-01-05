8a13ef31d63ac19736b0b7367892bc1296acbd12
krupal purohit <krupalpurohit1997@gmail.com>
2019-01-05 19:01:10 +0530

    added new function

--- feature_extraction_demo.py
+++ feature_extraction_demo.py
@@ -96,3 +96,70 @@ tfidf = tf * idf
 
 #show tfidf matrix
 display_features(np.round(tfidf,2),feature_names)
+
+#compute l2 norms
+norms = norm(tfidf,axis=1)
+
+print(np.round(norms,2))
+
+#compute normalized tfidf
+norm_tfidf = tfidf / norms[:,None]
+
+#show final
+display_features(np.round(norm_tfidf,2),feature_names)
+
+#compute new doc term freqs from bow freqs
+nd_tf = new_doc_features
+nd_tf = np.array(nd_tf,dtype='float64')
+
+#compute tfidf using idf matrix from train corpus
+nd_tfidf = nd_tf*idf
+nd_norms = norm(nd_tfidf,axis=1)
+norm_nd_tfidf = nd_tfidf / nd_norms[:,None]
+
+#show new_doc tfidf feature vector
+display_features(np.round(norm_nd_tfidf,2),feature_names)
+
+#generic function
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+def tfidf_extractor(corpus,ngram_range=(1,1)):
+    vectorizer = TfidfVectorizer(min_df=1,norm='l2',smooth_idf=True,use_idf=True,ngram_range=ngram_range)
+    features = vectorizer.fit_transform(corpus)
+    return vectorizer,features
+
+#build tfidf vector and get training corpus feature vectors
+tfidf_vectorizer,tfidf_features = tfidf_extractor(CORPUS)
+display_features(np.round(tfidf_features.todense(),2),feature_names)
+
+#get tfidf feature vector for the new doc
+nd_tfidf = tfidf_vectorizer.transform(new_doc)
+display_features(np.round(nd_tfidf.todense(),2),feature_names)
+
+#advance
+print('-------------------------------------------------------------')
+
+#advanced word vectorization models
+import gensim
+import nltk
+#tokenize corpora
+TOKENIZED_CORPUS =[nltk.word_tokenenize(sentence) for sentence in CORPUS]
+tokenized_new_doc = [nltk.word_tokenenize(sentence) for sentence in new_doc]
+
+#build the word2vec model on our training corpus
+model = gensim.models.word2vec(TOKENIZED_CORPUS,size=10,window=10,min_count=2,sample=1e-3)
+def average_word_vector(words,model,vocabulary,num_features):
+    feature_vector = np.Zeros((num_features,),dtype='float64')
+    nwords=0
+
+    for word in words:
+        if word in vocabulary:
+            nwords=nwords+1.
+            feature_vector=np.add(feature_vector,model[word])
+
+    if nwords:
+        feature_vector=np.divide(feature_vector,nwords)
+
+    return feature_vector
+
+#generalize above function for a corpus of document
